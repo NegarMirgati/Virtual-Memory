bc2e377414fb29d369f614e3921b8be86176cfd5
Negar Mirgati <negar.nbj96@yahoo.com>
2018-01-09 19:13:26 +0330

    some functions added

--- part1.cpp
+++ part1.cpp
@@ -1,11 +1,6 @@
 
 #include "part1.h"
 
- int phys_mem[MM_SIZE];
- int v_mem[VM_SIZE][2];
- int tlb[TLB_ENTRIES][2];
-
-
 using namespace std;
 
 
@@ -44,6 +39,59 @@ void init_tlb(){}
 
 void run_vmm(char* addr){
 
+
+	cout << " Running Virtual Memory Manger ..." << endl;
+
 	ifstream infile;
+	string line;
 	infile.open(addr);
+
+	while(getline(infile, line)){
+
+		int offset = get_offset(line);
+		int page_table_num = get_page_number(line);
+
+		//TO-DO
+	}
+}
+
+int get_offset(string addr){
+ 
+ 	return ( atoi(addr.c_str()) & 127);  
+}
+
+int get_page_number(string addr){
+
+	return (atoi(addr.c_str() ) & (127 >> 8));
+}
+
+bool is_in_tlb(int page_num){
+
+	bool hit = false;
+	int i = 0;
+
+	for(i = 0; i < TLB_ENTRIES; i++){
+
+		if(tlb[i][0] == page_num){
+
+			cout << " page number #"<<page_num << " was found in TLB " << endl;
+			hit = true;
+			break;
+		}
+	}
+	return hit;
+	/*if(!hit)
+		return -1;
+	else
+		return i;*/
+}
+
+int calc_phys_addr(int page_table_num, int offset){
+
+	int phys_addr=( phys_mem[v_mem[page_table_num][0]] * FRAME_SIZE) + offset;
+
+	cout << " calculated phys addr is " << phys_addr << endl;
+
+	return phys_addr;
+
 }
--- part1.h
+++ part1.h
@@ -2,11 +2,27 @@
 #include <fstream>
 #include <stdio.h>
 #include <cstdlib>
+#include <string>
 
 #define PAGE_SIZE 256
+#define FRAME_SIZE 256
+
 #define TLB_ENTRIES 16
+
 #define MM_SIZE 128
 #define VM_SIZE 256
+
+
+#define tlb_rw 0.5
+#define mm_rw 100
+#define disk_rw 250000
+
+int phys_mem[MM_SIZE];
+int v_mem[VM_SIZE][2];
+int tlb[TLB_ENTRIES][2];
+
+int num_of_tlb_hits = 0;
+int num_of_page_faults = 0;
 
 void init_vm();
 void init_phys_mem();
@@ -14,3 +30,7 @@ void init_tlb();
 bool check_arg(int argc, char* argv[]);
 void print_statistics();
 void run_vmm(char* addr);
+int get_offset(std::string addr);
+int get_page_num(std::string addr);
+bool is_in_tlb(int page_num);
+int calc_phys_addr(int page_num, int offset);
